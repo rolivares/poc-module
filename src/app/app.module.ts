dc98840fc805b29a3f2fdb3a11ba884025d8ce3e
Rodrigo Olivares <rodrigo.olivares@basedos.cl>
2024-07-29 11:49:06 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { ModalModule } from 'ngx-bootstrap/modal'
 
 console.log('outside', document.querySelector('meta[name="csp-nonce"]')?.getAttribute('content'))
 
@@ -15,8 +14,7 @@ console.log('outside', document.querySelector('meta[name="csp-nonce"]')?.getAttr
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    ModalModule.forRoot()
+    BrowserAnimationsModule
   ],
   providers: [
     {
